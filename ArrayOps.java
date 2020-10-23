5df0cb1e93c82d4b505229d37eca26986600e406
Timothy841 <tsit20@stuy.edu>
2020-10-23 12:13:15 -0400

    1st method works

---
+++ ArrayOps.java
@@ -0,0 +1,11 @@
+public class ArrayOps{
+
+  public static int sum(int[] arr) {
+    int a = 0;
+    for (int i = 0; i< arr.length; i++){
+      a+=arr[i];
+    }
+    return a;
+  }
+
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+
+  public static void main(String[] args){
+    int[] a = {2,4,6};
+    System.out.println(ArrayOps.sum(a));
+  }
+}
