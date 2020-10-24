40b2a2d3d169a5c166bfc402806d684da3997082
Timothy841 <tsit20@stuy.edu>
2020-10-24 18:45:05 -0400

    Third method works. At first, forgot about arrToString

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,5 +1,27 @@
 public class ArrayOps{
 
+  public static String arrToString(int[] arr){
+    String a = "{";
+    for (int i = 0; i<arr.length;i++){
+      if (i<arr.length-1){
+        a+=arr[i]+", ";
+      }
+      else{
+        a+=arr[i];
+      }
+    }
+    a+="}, ";
+    return a;
+  }
+
+  public static String arrayDeepToString(int[][] arr){
+    String a = "";
+    for (int i = 0; i<arr.length; i++){
+      a+=arrToString(arr[i]);
+    }
+    return a;
+  }
+
   public static int sum(int[] arr) {
     int a = 0;
     for (int i = 0; i< arr.length; i++){
@@ -12,6 +34,14 @@ public class ArrayOps{
     int a = 0;
     for (int i = 0; i< arr.length && arr[i]>a; i++){
       a = arr[i];
+    }
+    return a;
+  }
+
+  public static int[] sumRows(int[][] matrix){
+    int[] a = new int[matrix.length];
+    for (int i = 0; i< a.length; i++){
+      a[i] = sum(matrix[i]);
     }
     return a;
   }
--- Tester.java
+++ Tester.java
@@ -2,7 +2,12 @@ public class Tester{
 
   public static void main(String[] args){
     int[] a = {2,4,6};
+    int[] b = {3,7,5};
+    int[] c = {8,20,4};
+    int[] d = {14,3,9};
+    int[][] e = {a,b,c,d};
     System.out.println(ArrayOps.sum(a));
     System.out.println(ArrayOps.largest(a));
+    System.out.println(ArrayOps.arrToString(ArrayOps.sumRows(e)));
   }
 }
